371f6e74b97f37e2e30337168549097128273559
avesk <averykushner@gmail.com>
2016-03-26 01:58:00 -0700

    Before: .docx api

--- TextEdit.java
+++ TextEdit.java
@@ -1,6 +1,12 @@
 
 package texteditor_assignment_3;
 
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.event.*;
@@ -8,25 +14,28 @@ import java.awt.*;
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
-/**
- *
- * @author averykushner
- */
+
 
 public class TextEdit implements ActionListener {
     
     JTextArea jta;
     JTextField jtf;
     JEditorPane jep;
-    JButton openButton, findButton, helpButton, wordCountButton, charCountButton;
+    JButton openButton, findButton, replaceButton, helpButton, wordCountButton, charCountButton, sanSerifButton,
+            dialogButton;
     JLabel test;
     String contents = "";
-    JTextPane jpn;
+    JTextField setFontSize;
     JToolBar jtbr;
     JScrollPane jscrl;
     JMenuBar jmenu;
+    JMenu fontMenu, fontSize;
     //JMenu file
     //JMenuItem open, help, find, wordCount, charCount;
     
@@ -50,33 +59,71 @@ public class TextEdit implements ActionListener {
        
        JPanel content = new JPanel();
        
-       content.setBackground(Color.white);
+       content.setBackground(Color.lightGray);
        
        jmenu = new JMenuBar();
+       fontMenu = new JMenu("Font");
       
-       test = new JLabel("Default");
+       
+       JButton fontSize = new JButton("Font size");
+       
+       /* CHANGE FONT SIZE */
+       fontSize.addActionListener(new ActionListener(){ 
+           
+           @Override
+           public void actionPerformed(ActionEvent ae) {
+               String s = JOptionPane.showInputDialog(fontMenu, "Enter a text size");
+               int size = 0;
+           try{
+           Integer i = Integer.parseInt(s);
+            size = i;
+           }catch(NumberFormatException e){
+               JOptionPane.showMessageDialog(fontSize, "Please enter an integer value");
+               return;
+           }
+           jep.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,size));
+           }
+       });
        
        openButton = new JButton("Open");
        findButton = new JButton("Find");
-       helpButton = new JButton("Help");
        wordCountButton = new JButton("word count");
        charCountButton = new JButton("character count");
+       replaceButton = new JButton("Replace");
+       helpButton = new JButton("Help");
+       sanSerifButton = new JButton("sanserif");
+       dialogButton = new JButton("bold");
+       
        
        openButton.addActionListener(this);
        findButton.addActionListener(this);
        helpButton.addActionListener(this);
        wordCountButton.addActionListener(this);
        charCountButton.addActionListener(this);
+       replaceButton.addActionListener(this);
+       
+      // fontMenu.add(sanSerifButton);
+       //fontMenu.add(dialogButton);
+       
+       sanSerifButton.addActionListener((ae) ->{
+       jep.setFont(new Font(Font.SANS_SERIF,3,20));
+   });
+       dialogButton.addActionListener((ae) ->{
+       jep.setFont(new Font(Font.SERIF,1,10));
+   });
        
        jmenu.add(openButton);
        jmenu.add(findButton);
-       jmenu.add(helpButton);
+       jmenu.add(replaceButton);      
        jmenu.add(wordCountButton);
        jmenu.add(charCountButton);
+      // jmenu.add(fontMenu);
+       jmenu.add(fontSize);
+       jmenu.add(helpButton);
        
        jep = new JEditorPane();
        
-       jep.setPreferredSize(new Dimension(1000, 800));
+       jep.setPreferredSize(new Dimension(700, 800));
        jep.setText(" ");
        
        
@@ -91,6 +138,37 @@ public class TextEdit implements ActionListener {
        jfrm.setVisible(true);
        
        content.add(jep);
+       
+        /* SET TEXT CURSOR */
+       jep.addMouseMotionListener(new MouseMotionListener(){
+           @Override
+           public void mouseDragged(MouseEvent e) {
+           }
+
+           @Override
+           public void mouseMoved(MouseEvent e) {
+               jep.setCursor(new Cursor(Cursor.TEXT_CURSOR));
+               
+           }
+           
+       
+       });
+      
+       
+       /* SET MENU CURSOR */
+       jmenu.addMouseMotionListener(new MouseMotionListener(){
+           @Override
+           public void mouseDragged(MouseEvent e) {
+           }
+
+           @Override
+           public void mouseMoved(MouseEvent e) {
+               jmenu.setCursor(new Cursor(Cursor.HAND_CURSOR));
+           }
+       
+       });
+       
+       
        
        
    }
@@ -157,7 +235,7 @@ public class TextEdit implements ActionListener {
         for(String s : content){
         
         if(s.equals(f)){
-          choice = JOptionPane.showConfirmDialog(findButton, "found " + f + "(" +idx + ")" + " Find next? ");
+          choice = JOptionPane.showConfirmDialog(findButton, "found " + f + "(" +idx+1 + ")" + " Find next? ");
             idx ++;     
           }
           
@@ -166,6 +244,55 @@ public class TextEdit implements ActionListener {
        
         JOptionPane.showMessageDialog(findButton, f + " not found");
         }
+      
+      /*
+      
+      REPLACE
+      
+      */
+      
+       if(e.getActionCommand().equals("Replace")){
+           int choice = 0;
+           int idx=0;
+           String update = "";
+           
+          String temp = JOptionPane.showInputDialog(replaceButton, "Replace:");
+          String repl = JOptionPane.showInputDialog(replaceButton, "With:");
+          int i = 0;
+          String jeptext = "";
+          do{
+          
+          jeptext = jep.getText();
+          System.out.println(jeptext);
+          i = jeptext.indexOf(temp);
+          if(i==-1){ JOptionPane.showMessageDialog(replaceButton, "Not Found");
+          break;
+          }
+          
+          
+            
+          Document docContent = jep.getDocument();
+           
+            try {
+                docContent.remove(i, temp.length());
+                System.out.println("the length of the string = " + temp.length());
+                
+                } catch (BadLocationException ex) {
+                Logger.getLogger(TextEdit.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                docContent.insertString(i, repl, null);
+            } catch (BadLocationException ex) {
+                Logger.getLogger(TextEdit.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+           choice = JOptionPane.showConfirmDialog(replaceButton, " Replace next? ");
+           if(choice==1) break;
+            i = 0;
+            
+          }while(true);
+           
+       }
       
       /*
         
@@ -193,6 +320,23 @@ public class TextEdit implements ActionListener {
        if(t.length()==0) JOptionPane.showMessageDialog(charCountButton, 0);
        else JOptionPane.showMessageDialog(charCountButton, t.length()-1);
 
+      }
+      
+      /*
+      
+      HELP 
+      
+      */
+      
+      if(e.getActionCommand().equals("Help")){
+          JOptionPane.showMessageDialog(helpButton, "Open: select a .txt or a .java file to open in editor \n"
+                  + "Find: Enter a word that will be found \n"
+                  + "Replace: Enter first word as the word you would like to be replaced, and the second word as the "
+                  + "word you would like to replace the first word with \n"
+                  + "Word count: Retrieves the number of words in document"
+                  + "Character count: Retreives the number of characters in the document"
+                  , "HELP", 1
+                  );
       }
       
 
